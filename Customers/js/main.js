a15fed9a9a4b23e815fb7f835e41e2aa16e259e0
rendaniluk <lukhwa11582924>
2017-06-02 09:56:33 +0200

    Address book for

--- Customers/js/main.js
+++ Customers/js/main.js
@@ -1,62 +1,154 @@
-$(document).ready(function () { // wait for document to be ready
+$(document).ready(function() { // wait for document to be ready
 
-    var req = {}; // empty object to hold our http request
-    req.deviceName = 'aisTester'; // <<---- here change to a unique name
-    req.username = "demo";
-    req.password = "demo";
+  $.getJSON('./js/customers.js')
+    .error(function(err) {
+      console.log(err);
+    })
+    .done(function(data) {
+      // console.log($(".results").innerHTML);
+      // console.log(JSON.stringify(data)) // <<- log data to console
+      // const CompanyData = JSON.stringify(data["title"]);
+      var myData = data.fs_DATABROWSE_F0101.data.gridData.rowset.map(
+        function(record) {
+          return {
+            alphaName: record.F0101_ALPH,
+            type: record.F0101_AT1
 
-    // authenticate with the system by getting a token
-    $.ajax({
-        url: "http://demo.steltix.com/jderest/tokenrequest", // <<- JD Edwards API token service
-        type: 'post', // <<- the method that we using
-        data: JSON.stringify(req), // <<- JSON of our request obj
-        contentType: 'application/json', // <<- telling server how we are going to communicate
-        fail: function (xhr, textStatus, errorThrown) {
+          }
+        });
 
-            console.log(errorThrown, textStatus, xhr) //  <<- log any http errors to the console
-
+      var newData = [];
+      for (var i = 0; i < myData.length; i++) {
+        var customerData = myData[i];
+        var resultingData = customerData.type;
+        if (resultingData.includes('O')) {
+          newData.push(customerData);
         }
-    }).done(function (data, textStatus, xhr) {
-
-        if (data.hasOwnProperty('userInfo')) {  // <<- see example response below
-
-            var token = data.userInfo.token;
-
-            // build a request obj to fetch data
-            var reqData = {  
-                    "deviceName" : "aisTester",
-                    "targetName" : "F0101",
-                    "targetType" : "table",
-                    "outputType":"GRID_DATA",
-                    "dataServiceType" : "BROWSE",  
-                    "maxPageSize" : "100",
-                    "query" : {
-                        "autoFind" : true,
-                        "condition" : []
-                        }
-                    }
-
-
-            reqData.token = token;  // <<- add our token from 1st request
-
-            $.ajax({
-                url: "http://demo.steltix.com/jderest/dataservice", // <<- can also try http://demo.steltix.com/jderest/formservice with example request object below"
-                type: "post",
-                contentType: "application/json",
-                data: JSON.stringify(reqData)
-            }).done(function (data) {
-
-                console.log(JSON.stringify(data)) // <<- log data to console
-                const resultsElem = document.querySelector('.results');  // <<- handle for results
-                resultsElem.textContent = JSON.stringify(data);  // <<-  add data to DOM
-                
-            })
-
+        if (resultingData.includes('C')) {
+          newData.push(customerData);
         }
+        if (resultingData.includes('P')) {
+          newData.push(customerData);
+        }
+        if (resultingData.includes('V')) {
+          newData.push(customerData);
+        }
+        if (resultingData.includes('D')) {
+          newData.push(customerData);
+        }
+        if (resultingData.includes('W')) {
+          newData.push(customerData);
+        }
+      }
+
+
+
+      var tempScript = document.querySelector('.alphaAt1');
+      var compTemp = Handlebars.compile(tempScript.innerHTML);
+      var tempResults = compTemp({
+        myData: newData
+      });
+      var resultsElem = document.querySelector('.results'); // <<- handle for results
+      resultsElem.innerHTML = tempResults; // <<-  add data to DOM
+      console.log(newData);
+      var opportunity = [];
+      var customer = [];
+      var prospect = [];
+      var supplier = [];
+      var competitor = [];
+      var warehouse = [];
+
+      for (var i = 0; i < newData.length; i++) {
+        var partnerShip = newData[i];
+        if (partnerShip.type === 'O') {
+          opportunity.push(partnerShip);
+        } else if (partnerShip.type === 'C') {
+          customer.push(partnerShip);
+        } else if (partnerShip.type === 'P') {
+          prospect.push(partnerShip);
+        } else if (partnerShip.type === 'V') {
+          supplier.push(partnerShip);
+        } else if (partnerShip.type === 'D') {
+          competitor.push(partnerShip);
+        } else if (partnerShip.type === 'W') {
+          warehouse.push(partnerShip);
+        }
+      }
+
+      var inputBox = document.querySelector('.input');
+      var input = inputBox.value;
+      inputBox.addEventListener('keyup', newData.filter(function(input){
+            resultsElem.innerHTML = tempResults = compTemp({
+              myData: newData
+            });
+          return newData;
+      }));
+
+      var array = [opportunity.length, customer.length, prospect.length,
+        supplier.length, competitor.length, warehouse.length
+      ];
+console.log(array);
 
     })
 
-})
+
+  // var req = {}; // empty object to hold our http request
+  // req.deviceName = 'aisTester'; // <<---- here change to a unique name
+  // req.username = "demo";
+  // req.password = "demo";
+  //
+  // // authenticate with the system by getting a token
+  // $.ajax({
+  //     url: "http://demo.steltix.com/jderest/tokenrequest", // <<- JD Edwards API token service
+  //     type: 'post', // <<- the method that we using
+  //     data: JSON.stringify(req), // <<- JSON of our request obj
+  //     contentType: 'application/json', // <<- telling server how we are going to communicate
+  //     fail: function (xhr, textStatus, errorThrown) {
+  //
+  //         console.log(errorThrown, textStatus, xhr) //  <<- log any http errors to the console
+  //
+  //     }
+  // }).done(function (data, textStatus, xhr) {
+  //
+  //     if (data.hasOwnProperty('userInfo')) {  // <<- see example response below
+  //
+  //         var token = data.userInfo.token;
+  //
+  //         // build a request obj to fetch data
+  //         var reqData = {
+  //                 "deviceName" : "SiyaRenda01",
+  //                 "targetName" : "F0101",
+  //                 "targetType" : "table",
+  //                 "outputType":"GRID_DATA",
+  //                 "dataServiceType" : "BROWSE",
+  //                 "maxPageSize" : "100",
+  //                 "query" : {
+  //                     "autoFind" : true,
+  //                     "condition" : []
+  //                     }
+  //                 }
+  //
+  //
+  //         reqData.token = token;  // <<- add our token from 1st request
+  //
+  //         $.ajax({
+  //             url: "http://demo.steltix.com/jderest/dataservice", // <<- can also try http://demo.steltix.com/jderest/formservice with example request object below"
+  //             type: "post",
+  //             contentType: "application/json",
+  //             data: JSON.stringify(reqData)
+  //         }).done(function (data) {
+  //
+  //             console.log(JSON.stringify(data)) // <<- log data to console
+  //             const resultsElem = document.querySelector('.results');  // <<- handle for results
+  //             resultsElem.textContent = JSON.stringify(data,null,3);  // <<-  add data to DOM
+  //
+  //         })
+  //
+  //     }
+  //
+  // })
+
+});
 
 
 
